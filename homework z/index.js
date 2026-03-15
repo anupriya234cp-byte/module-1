8f659275fbc74f8283e09ce1aebcb7158b454438
anupriya234cp-byte <anupriya234.cp@gmail.com>
2026-03-15 17:17:51 +0530

    Add files via upload

---
+++ homework z/index.js
@@ -0,0 +1,6 @@
+function myFunction(){
+document.getElementById("result").innerHTML=
+document.getElementById("demo").firstChild.nodevalues;
+    document.getElementById("result1").innerHTML =
+    document.getElementById("demo").childNodes[0].nodevalues;
+}
